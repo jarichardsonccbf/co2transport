2a4ffd78ce4052be78a985860fddb8801232273c
Jason Richardson <jarichardson@cocacolaflorida.com>
2020-02-21 13:54:38 -0500

    Initial commit

---
+++ HANA_pull.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+library(RJDBC)
+library(keyring)
+library(lubridate)
+
+source("SP_pull.Rmd")
+
+# import and establish hana connection ----
+
+options(java.parameters = "-Xmx8048m")
+# memory.limit(size=10000000000024)
+
+# classPath="C:/Program Files/sap/hdbclient/ngdbc.jar"
+# For ngdbc.jar use        # jdbcDriver <- JDBC(driverClass="com.sap.db.jdbc.Driver",
+# For HANA Studio jar use  # jdbcDriver <- JDBC(driverClass="com.sap.ndb.studio.jdbc.JDBCConnection",
+
+jdbcDriver <- JDBC(driverClass="com.sap.db.jdbc.Driver",
+                   classPath="C:/Program Files/sap/hdbclient/ngdbc.jar")
+
+jdbcConnection <- dbConnect(jdbcDriver,
+                            "jdbc:sap://vlpbid001.cokeonena.com:30015/_SYS_BIC",
+                            "fl014036",
+                            key_get("hana.pw"))
+
+
+# Fetch all results
+
+# "SELECT
+# \"SalesOffice\",
+# \"_GCC_G_ADDKF1\" as \"Additional Key Figures 1\"
+# where
+# \"TradeNameText\" = ? OR
+# \"TradeNameText\" = ? OR
+# \"TradeNameText\" = ? OR
+# \"TradeNameText\" = ?
+
+sql <- "SELECT TO_Number(\"ActualDeliveryDate\") as \"HANA date\",
+\"DriverDesc\",
+\"DriverSupervisorName\",
+\"ShipmentNumber\" as \"HANA ship\",
+\"PackTypeDesc\",
+\"SalesOffice\" FROM \"cona-reporting.delivery-execution::Q_CA_S_DeliveryExecution\"(
+            'PLACEHOLDER' = ('$$IP_KeyDate$$', '202001')) where
+\"PackTypeDesc\" = ? AND
+\"ActualDeliveryDate\" > ?"
+
+param1 <- 'CO2 Tank'
+param2 <- '20190101'
+
+hana.shipments <- dbGetQuery(jdbcConnection, sql, param1, param2)
+
+dbDisconnect(jdbcConnection)
+
+rm(jdbcConnection, jdbcDriver, param1, param2, sql)
+
+# do join ----
+
+class(SP.shipments$`Shipment number`)
+
+SP.shipments$`Shipment number` <- unlist(SP.shipments$`Shipment number`)
+
+SP.shipments <- SP.shipments %>%
+  rename(SP.ship = `Shipment number`)
+
+df <- hana.shipments %>%
+  filter(!is.na(`HANA ship`)) %>%
+  mutate(`HANA ship` = substring(`HANA ship`, 3)) %>%
+  left_join(SP.shipments, by = c("HANA ship" = "SP.ship")) %>%
+  select(-c(PackTypeDesc, Date)) %>%
+  rename(Date = `HANA date`) %>%
+  mutate(MATCH = ifelse(is.na(`DSD Location`), "NO FORM", "FORM FILLED"),
+         Date = as.character(Date),
+         Date = ymd(Date))
+
+write.csv(df, "outputs/data.csv", row.names = FALSE)
---
+++ SP_pull.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 11:22:12 2020
+
+@author: FL014036
+
+"SharePointAdmin@cocacolaflorida.com", "P@$$word"
+"""
+
+from shareplum import Site
+from shareplum import Office365
+import pandas as pd
+
+
+authcookie = Office365('https://cocacolaflorida.sharepoint.com', username='SharePointAdmin@cocacolaflorida.com', password='P@$$word').GetCookies()
+site = Site('https://cocacolaflorida.sharepoint.com/Distribution/', authcookie=authcookie)
+sp_list = site.List('Hazardous Material Disclosure Statement') 
+data = sp_list.GetListItems()
+
+data = pd.DataFrame(data)
+
+data = data[['Date','Drivers Name', 'Shipment number', 'DSD Location']]
